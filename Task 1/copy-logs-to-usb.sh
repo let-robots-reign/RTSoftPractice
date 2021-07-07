090091e6e16f53b83e455cd3bbaed143c9939a96
Alexey Zotov <zotov.alexei.1@yandex.ru>
2021-07-07 10:27:58 +0300

    first iteration

--- Task 1/copy-logs-to-usb.sh
+++ Task 1/copy-logs-to-usb.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/bash
 
+mkdir /media/usb
+mount /dev/added_usb /media/usb
+
+cp -i /var/log/boot.log /media/usb/logs/boot.log
+cp -i /var/log/dmesg /media/usb/logs/dmesg.log
+
+umount /media/usb
+rm -r /media/usb
